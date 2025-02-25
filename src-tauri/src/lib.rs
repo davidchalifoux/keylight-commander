c77880e6c54fe44cddc537c26140577bf99624df
David Chalifoux <david@chalifoux.dev>
2025-02-25 17:57:04 -0500

    Setup updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -55,6 +55,7 @@ async fn scan() -> Vec<ElgatoService> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
