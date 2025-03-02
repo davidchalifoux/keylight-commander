5aec9305b6dc3a989a81c66ebc2692070462e426
David Chalifoux <david@chalifoux.dev>
2025-03-02 11:55:18 -0500

    fix: Fix light scheme handling

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -15,10 +15,10 @@ const Slider = React.forwardRef<
 		)}
 		{...props}
 	>
-		<SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-neutral-900/20 dark:bg-neutral-50/20">
-			<SliderPrimitive.Range className="absolute h-full bg-neutral-900 dark:bg-neutral-50" />
+		<SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-neutral-50/20">
+			<SliderPrimitive.Range className="absolute h-full bg-neutral-50" />
 		</SliderPrimitive.Track>
-		<SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-neutral-200 border-neutral-900/50 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-800 dark:focus-visible:ring-neutral-300" />
+		<SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border shadow transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-neutral-700 bg-neutral-800 focus-visible:ring-neutral-300" />
 	</SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
@@ -43,7 +43,7 @@ const TemperatureSlider = React.forwardRef<
 		>
 			{/* <SliderPrimitive.Range className="absolute h-full bg-neutral-900 dark:bg-neutral-50" /> */}
 		</SliderPrimitive.Track>
-		<SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-neutral-200 border-neutral-900/50 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-800 dark:focus-visible:ring-neutral-300" />
+		<SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border shadow transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-neutral-700 bg-neutral-800 focus-visible:ring-neutral-300" />
 	</SliderPrimitive.Root>
 ));
 TemperatureSlider.displayName = SliderPrimitive.Root.displayName;
